f3d04985270472adf572e9df67860a69115d0420
Álvaro M. Villar <144016805+amv87@users.noreply.github.com>
2023-11-19 19:55:02 +0000

    finished src/js/views/newcontact.js layout and styles

--- src/js/views/newcontact.js
+++ src/js/views/newcontact.js
@@ -1,14 +1,37 @@
-import React from "react";
-import "../../styles/contactlist.css";
+import React, { useState, useEffect, useContext } from "react";
+import { Link } from "react-router-dom";
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+import { Context } from "../store/appContext";
+
+import "../../styles/demo.css";
+
+export const Newcontact = () => {
+	const { store, actions } = useContext(Context);
+
+	return (
+		<div className="container">
+			<div class="mb-3">
+				<label for="fullName" class="form-label">Full Name</label>
+				<input type="text" class="form-control" id="fullName" placeholder="Full Name" />
+			</div>
+			<div class="mb-3">
+				<label for="email" class="form-label">Email</label>
+				<input type="text" class="form-control" id="email" placeholder="Email" />
+			</div>
+			<div class="mb-3">
+				<label for="phone" class="form-label">Phone</label>
+				<input type="text" class="form-control" id="phone" placeholder="Phone" />
+			</div>
+			<div class="mb-3">
+				<label for="address" class="form-label">Address</label>
+				<input type="text" class="form-control" id="address" placeholder="Address" />
+			</div>
+			<div class="d-grid gap-2">
+				<button class="btn btn-primary" type="button">Save</button>
+			</div>
+			<Link to="/">
+				<button className="btn text-primary">Or get back to contacts</button>
+			</Link>
+		</div>
+	);
+};

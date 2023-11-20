164e9c5dda62ef5cab462d2ce98b038dd3094dfb
Álvaro M. Villar <144016805+amv87@users.noreply.github.com>
2023-11-20 20:55:39 +0000

    updated flux with API GET and DELETE functionality

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -13,26 +13,7 @@ const getState = ({ getStore, getActions, setStore }) => {
 					initial: "white"
 				}
 			],
-			contacts: [
-				{
-					name: "Mike Anamendolla",
-					email: "mike.ana@example.com",
-					phone: "(870) 288-4149",
-					address: "5844 Hillcrest Rd"
-				},
-				{
-					name: "John Mayers",
-					email: "mike.ana@example.com",
-					phone: "(870) 288-4149",
-					address: "5844 Hillcrest Rd"
-				},
-				{
-					name: "Peter Watkins",
-					email: "mike.ana@example.com",
-					phone: "(870) 288-4149",
-					address: "5844 Hillcrest Rd"
-				}
-			]
+			contacts: []
 		},
 		actions: {
 			// Use getActions to call a function within a fuction
@@ -43,6 +24,32 @@ const getState = ({ getStore, getActions, setStore }) => {
 				/**
 					fetch().then().then(data => setStore({ "foo": data.bar }))
 				*/
+				const store = getStore();
+				var requestOptions = {
+					method: 'GET',
+					redirect: 'follow'
+				  };
+				  
+				  fetch("https://playground.4geeks.com/apis/fake/contact/agenda/amv87", requestOptions)
+					.then(response => response.json())
+					.then(data => setStore({ contacts: data }))
+					.catch(error => console.log('error', error));
+			},
+			deleteContact: (indexToDelete) => {
+				//get the store
+				const store = getStore();
+
+				var requestOptions = {
+					method: 'DELETE',
+					redirect: 'follow'
+				  };
+				  
+				  fetch("https://playground.4geeks.com/apis/fake/contact/" + indexToDelete, requestOptions)
+					.then(response => response.json())
+					.then(data => setStore({ contacts: data }))
+					.catch(error => console.log('error', error));
+					
+				  
 			},
 			changeColor: (index, color) => {
 				//get the store

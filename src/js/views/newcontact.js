447c3b971ef9b6f8389d2dfb0a84b4837927515d
Álvaro M. Villar <144016805+amv87@users.noreply.github.com>
2023-11-20 20:56:16 +0000

    updated styles

--- src/js/views/newcontact.js
+++ src/js/views/newcontact.js
@@ -11,7 +11,6 @@ export const Newcontact = () => {
 	const [inputEmail, setInputEmail] = useState('');
 	const [inputPhone, setInputPhone ] = useState('');
 	const [inputAdress, setInputAdress ] = useState('');
-	const [newContact, setNewContact] = useState([]);
 
 	function addNewarray() {
 		if (inputName !== '' && inputEmail !== '' && inputPhone !== '' && inputAdress !== '') {
@@ -30,27 +29,27 @@ export const Newcontact = () => {
 	return (
 		<div className="container">
 			<h1 className="text-center mt-3">Add a new contact</h1>
-			<div class="mb-3">
-				<label for="fullName" class="form-label">Full Name</label>
-				<input type="text" class="form-control" id="fullName" placeholder="Full Name" value={inputName} onChange={(e)=>setInputName(e.target.value)} />
+			<div className="mb-3">
+				<label htmlFor="fullName" className="form-label">Full Name</label>
+				<input type="text" className="form-control" id="fullName" placeholder="Full Name" value={inputName} onChange={(e)=>setInputName(e.target.value)} />
 			</div>
-			<div class="mb-3">
-				<label for="email" class="form-label">Email</label>
-				<input type="text" class="form-control" id="email" placeholder="Email" value={inputEmail} onChange={(e)=>setInputEmail(e.target.value)} />
+			<div className="mb-3">
+				<label htmlFor="email" className="form-label">Email</label>
+				<input type="text" className="form-control" id="email" placeholder="Email" value={inputEmail} onChange={(e)=>setInputEmail(e.target.value)} />
 			</div>
-			<div class="mb-3">
-				<label for="phone" class="form-label">Phone</label>
-				<input type="text" class="form-control" id="phone" placeholder="Phone" value={inputPhone} onChange={(e)=>setInputPhone(e.target.value)} />
+			<div className="mb-3">
+				<label htmlFor="phone" className="form-label">Phone</label>
+				<input type="text" className="form-control" id="phone" placeholder="Phone" value={inputPhone} onChange={(e)=>setInputPhone(e.target.value)} />
 			</div>
-			<div class="mb-3">
-				<label for="address" class="form-label">Address</label>
-				<input type="text" class="form-control" id="address" placeholder="Address" value={inputAdress} onChange={(e)=>setInputAdress(e.target.value)} />
+			<div className="mb-3">
+				<label htmlFor="address" className="form-label">Address</label>
+				<input type="text" className="form-control" id="address" placeholder="Address" value={inputAdress} onChange={(e)=>setInputAdress(e.target.value)} />
 			</div>
-			<div class="d-grid gap-2">
-				<button class="btn btn-primary" type="button" onClick={()=>(addNewarray())}>Save</button>
+			<div className="d-grid gap-2">
+				<button className="btn btn-primary" type="button" onClick="" >Save</button>
 			</div>
 			<Link to="/">
-				<button className="btn text-primary">Or get back to contacts</button>
+				<button className="btn text-primary mb-3">Or get back to contacts</button>
 			</Link>
 		</div>
 	);

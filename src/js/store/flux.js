16ecf9be278c0a87eb348884db08c59edf3b67bd
Álvaro M. Villar <144016805+amv87@users.noreply.github.com>
2023-11-27 22:44:02 +0000

    added editcontact function

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -46,10 +46,62 @@ const getState = ({ getStore, getActions, setStore }) => {
 				  
 				  fetch("https://playground.4geeks.com/apis/fake/contact/" + indexToDelete, requestOptions)
 					.then(response => response.json())
-					.then(data => setStore({ contacts: store.contacts.filter((item)=>item.id!==indexToDelete) }))
+					.then(data => setStore({ contacts: store.contacts.filter((item)=> item.id !== indexToDelete) }))
 					.catch(error => console.log('error', error));
 					
 				  
+			},
+			editContact: (inputName, inputEmail, inputPhone, inputAdress, indexToEdit) => {
+				var myHeaders = new Headers();
+				myHeaders.append("Content-Type", "application/json");
+
+				var raw = JSON.stringify({
+				"address": inputAdress,
+				"agenda_slug": "amv87",
+				"email": inputEmail,
+				"full_name": inputName,
+				"phone": inputPhone
+				});
+
+				var requestOptions = {
+				method: 'PUT',
+				headers: myHeaders,
+				body: raw,
+				redirect: 'follow'
+				};
+
+				fetch("https://playground.4geeks.com/apis/fake/contact/" + indexToEdit, requestOptions)
+				.then(response => response.text())
+				.then(result => console.log(result))
+				.then(data => loadSomeData())
+				.catch(error => console.log('error', error));
+			},
+			createContact: (inputName, inputEmail, inputPhone, inputAdress) => {
+				console.log('Im working')
+
+				var myHeaders = new Headers();
+				myHeaders.append("Content-Type", "application/json");
+
+				var raw = JSON.stringify({
+				"address": inputAdress,
+				"agenda_slug": "amv87",
+				"email": inputEmail,
+				"full_name": inputName,
+				"phone": inputPhone
+				});
+
+				var requestOptions = {
+				method: 'POST',
+				headers: myHeaders,
+				body: raw,
+				redirect: 'follow'
+				};
+
+				fetch("https://playground.4geeks.com/apis/fake/contact/", requestOptions)
+				.then(response => response.json())
+				.then(data => loadSomeData())
+				.catch(error => console.log('error', error));
+
 			},
 			changeColor: (index, color) => {
 				//get the store

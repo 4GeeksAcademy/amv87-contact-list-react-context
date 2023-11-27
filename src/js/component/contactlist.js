143e85e9384dc0cbf7e2a1f96b3e352be4d52782
Álvaro M. Villar <144016805+amv87@users.noreply.github.com>
2023-11-27 22:45:22 +0000

    added edit contact path

--- src/js/component/contactlist.js
+++ src/js/component/contactlist.js
@@ -36,12 +36,14 @@ export const Contactlist = () => {
 										</svg> {item.email}</p>
 								</div>
 								<div className="col-2 d-flex align-items-center justify-content-evenly">
-									<button className="btn text-dark" onClick="">
-										<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-pencil-fill" viewBox="0 0 16 16">
-  										<path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"/>
-										</svg>
-									</button>
-									<button className="btn text-dark" onClick="" data-bs-toggle="modal" data-bs-target="#deleteModal">
+									<Link to="/Editcontact">
+										<button className="btn text-dark">
+											<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-pencil-fill" viewBox="0 0 16 16">
+											<path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"/>
+											</svg>
+										</button>
+									</Link>
+									<button className="btn text-dark" data-bs-toggle="modal" data-bs-target="#deleteModal">
 										<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-trash-fill" viewBox="0 0 16 16">
   										<path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
 										</svg>
@@ -58,7 +60,9 @@ export const Contactlist = () => {
 												</div>
 												<div className="modal-footer">
 													<button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Oh no!</button>
-													<button type="button" className="btn btn-primary" onClick={()=>actions.deleteContact(item.id)} data-bs-dismiss="modal" >Yes baby!</button>
+													<Link to="/">
+														<button type="button" className="btn btn-primary" onClick={()=>actions.deleteContact(item.id)} data-bs-dismiss="modal" >Yes baby!</button>
+													</Link>
 												</div>
 											</div>
 										</div>

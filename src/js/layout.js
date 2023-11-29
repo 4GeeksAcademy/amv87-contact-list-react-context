af50142bf8c5529abc469500a5c61888aad2fcb6
Álvaro M. Villar <144016805+amv87@users.noreply.github.com>
2023-11-29 21:34:07 +0000

    cleared src/js/layout.js

--- src/js/layout.js
+++ src/js/layout.js
@@ -1,18 +1,11 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ScrollToTop from "./component/scrollToTop";
-
-import { Home } from "./views/home";
-import { Demo } from "./views/demo";
-import { Single } from "./views/single";
 import injectContext from "./store/appContext";
-
-import { Navbar } from "./component/navbar";
 import { Contactlist } from "./component/contactlist";
 import { Newcontact } from "./views/newcontact";
 import { Editcontact } from "./views/editcontact";
 
-//create your first component
 const Layout = () => {
 	const basename = process.env.BASENAME || "";
 

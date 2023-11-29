dcf197c25a1f421caab4abecb22038e5a287decd
Álvaro M. Villar <144016805+amv87@users.noreply.github.com>
2023-11-29 21:36:25 +0000

    added alert and usestate values to input

--- src/js/views/editcontact.js
+++ src/js/views/editcontact.js
@@ -2,19 +2,17 @@ import React, { useState, useEffect, useContext } from "react";
 import { Link, useParams } from "react-router-dom";
 import PropTypes from "prop-types";
 import { Context } from "../store/appContext";
-import "../../styles/demo.css";
 
 export const Editcontact = () => {
 	const { store, actions } = useContext(Context);
 	const params = useParams();
-	const [inputName, setInputName ] = useState();
-	const [inputEmail, setInputEmail] = useState();
-	const [inputPhone, setInputPhone ] = useState();
-	const [inputAdress, setInputAdress ] = useState();
+	const [inputName, setInputName ] = useState(store.contacts[params.theid].full_name);
+	const [inputEmail, setInputEmail] = useState(store.contacts[params.theid].email);
+	const [inputPhone, setInputPhone ] = useState(store.contacts[params.theid].phone);
+	const [inputAdress, setInputAdress ] = useState(store.contacts[params.theid].address);
 
 	return (
 		<div className="container">
-					
 			<h1 key={store.contacts[params.theid].id} className="text-center mt-3">Edit a contact</h1>
 			<div className="mb-3">
 				<label htmlFor="fullName" className="form-label">Full Name</label>
@@ -34,13 +32,17 @@ export const Editcontact = () => {
 			</div>
 			<Link to="/">
 				<div className="d-grid gap-2">
-					<button  className="btn btn-primary" type="button" onClick={()=>actions.editContact(inputName, inputEmail, inputPhone, inputAdress, store.contacts[params.theid].id)} >Save</button>
+					<button
+						className="btn btn-primary"
+						type="button"
+						onClick={()=>(inputName!=='' && inputEmail!=='' && inputPhone!=='' && inputAdress!=='' ? actions.editContact(inputName, inputEmail, inputPhone, inputAdress, store.contacts[params.theid].id) : alert('You must complete all fields! Your contact has not been saved.'))} >
+						Save
+					</button>
 				</div>
 			</Link>
 			<Link to="/">
 				<button className="btn text-primary mb-3">Or get back to contacts</button>
 			</Link>
-		
 		</div>
 	);
 };
